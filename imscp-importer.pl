f2798d2cf6cae8f58aa9dd82e2504826c51b5fb3
Laurent Declercq <l.declercq@nuxwin.com>
2014-06-16 19:46:14 +0200

    In progress...

--- imscp-importer.pl
+++ imscp-importer.pl
@@ -1,57 +1,28 @@
 #!/usr/bin/perl
 
+# i-MSCP Data Importer
+# Copyright (C) 2014 Laurent Declercq <l.declercq@nuxwin.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+
 use strict;
 use warnings;
-
 use FindBin;
 use lib "$FindBin::Bin/lib", '/var/www/imscp/engine/PerlLib';
+use iMSCP::DataImporter::Application;
 
-use iMSCP::Debug;
-use iMSCP::Requirements;
-use iMSCP::Bootstrapper;
-use iMSCP::Dialog;
-
-use version;
-
-
-
-$ENV{'LC_MESSAGES'} = 'C';
-$ENV{'IMSCP_CLEAR_SCREEN'} = 0;
-
-# Global variable that holds questions
-%main::questions = () if ! %main::questions;
-
-newDebug('imscp-importer.log');
-
-# Entering in silent mode
-silent(1);
-
-# Bootstrap environment
-my $bootstrapper = iMSCP::Bootstrapper->getInstance();
-$bootstrapper->boot();
-
-# Check for i-MSCP processes
-for(
-	'imscp-backup-all', 'imscp-backup-imscp', 'imscp-dsk-quota', 'imscp-srv-traff', 'imscp-vrl-traff',
-    'awstats_updateall.pl'
-) {
-	if(! $bootstrapper->getInstance()->lock("/tmp/$_.lock", 'nowait')) {
-		iMSCP::Dialog->factory()->msgbox(<<EOF);
-
-At least one i-MSCP process is currently running on your system. You must wait until the end of any i-MSCP process.
-
-EOF
-		exit 0;
-	}
-}
-
-# Check for i-MSCP version
-if($main::imscpConfig{'BuildDate'} < 20140608 && qv($main::imscpConfig{'Version'} < qv('v1.1.11') {
-	iMSCP::Dialog->factory()->msgbox(<<EOF);
-
-    Your i-MSCP version is not compatible with this importer. Please update your system to i-MSCP 1.1.11 first.
-
-EOF
-}
+iMSCP::DataImporter::Application->getInstance()->run();
 
 exit 0;
